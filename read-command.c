b35c9c9e645302a6bc50e6ada76e2ae75cea4cc9
Taylor Lee <tay333@gmail.com>
2012-04-12 16:07:38 -0700

    repush

--- read-command.c
+++ read-command.c
@@ -88,7 +88,10 @@ command_t traverse_stream( command_t head, bool *subtree_complete )
             command_ptr = traverse_stream(head->u.command[0],subtree_complete);
             if( *subtree_complete )
             {
-                free(head->u.command[0]);
+                if (head->u.command[0]->type == SEQUENCE_COMMAND || head->u.command[0]->type == SUBSHELL_COMMAND )
+                {
+                    free(head->u.command[0]);
+                }
                 head->u.command[0] = NULL;
             }
             *subtree_complete = false;
@@ -99,7 +102,10 @@ command_t traverse_stream( command_t head, bool *subtree_complete )
             command_ptr = traverse_stream(head->u.command[1],subtree_complete);
             if( *subtree_complete )
             {
-                free(head->u.command[1]);
+                if (head->u.command[1]->type == SEQUENCE_COMMAND || head->u.command[1]->type == SUBSHELL_COMMAND )
+                {
+                    free(head->u.command[0]);
+                }
                 head->u.command[1] = NULL;
             }
             *subtree_complete = true;
@@ -118,7 +124,10 @@ command_t traverse_stream( command_t head, bool *subtree_complete )
             command_ptr = traverse_stream(head->u.subshell_command,subtree_complete);
             if( *subtree_complete )
             {
-                free(head->u.subshell_command);
+                if (head->u.subshell_command->type == SEQUENCE_COMMAND || head->u.subshell_command->type == SUBSHELL_COMMAND )
+                {
+                    free(head->u.subshell_command);
+                }
                 head->u.subshell_command = NULL;
             }
             *subtree_complete = true;
@@ -423,7 +432,6 @@ command_t add_command_subshell( int (*get_next_byte) (void *), void *stream, boo
             }
             if ( next_byte == EOF )
             {
-                
                 continue;
             }
             else if( ! isspace(next_byte) && ! is_word_char(next_byte))  //error
@@ -612,6 +620,10 @@ command_t add_command_pipe( int (*get_next_byte) (void *), void *stream, command
             break;
         }
     }
+    if( byte == EOF)
+    {
+        fseek( stream, -1, SEEK_CUR );
+    }
     return command;
 }
     
@@ -640,7 +652,6 @@ command_t add_command_sequence( int (*get_next_byte) (void *), void *stream, com
             byte = get_next_byte( stream );
             if ( byte == '|' )      //OR command
             {
-                fseek( stream, -2, SEEK_CUR);
                 break;
             }
             else
@@ -752,4 +763,4 @@ command_t read_command_stream (command_stream_t s)
         s->head = NULL;
     }
     return command_ptr;
-n
+}
