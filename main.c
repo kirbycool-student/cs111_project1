4646d3818fbc7d5f0d2dba2820ab8f106f8b8e71
Taylor Lee <tay333@gmail.com>
2012-04-24 11:40:17 -0700

    prototypes and command_list population

--- main.c
+++ main.c
@@ -4,8 +4,16 @@
 #include <error.h>
 #include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "command.h"
+
+struct high_lvl_command
+{
+    command_t command;
+    char **inputs;
+    char **outputs;
+};
 
 static char const *program_name;
 static char const *script_name;
@@ -21,6 +29,12 @@ get_next_byte (void *stream)
 {
     return getc (stream);
 }
+
+// traverse high lvl command to prep io for dependency graph
+void consolidate_io(char **input, char**output)
+{
+    return;
+};
 
 int
 main (int argc, char **argv)
@@ -49,11 +63,30 @@ main (int argc, char **argv)
     FILE *script_stream = fopen (script_name, "r");
     if (! script_stream)
     error (1, errno, "%s: cannot open", script_name);
+    
     command_stream_t command_stream =
         make_command_stream (get_next_byte, script_stream);
-
     command_t last_command = NULL;
     command_t command;
+    
+    //define and allocate array of high lvl commands
+    int num_hl_commands = 100;//default
+    high_lvl_command_t command_list = malloc(sizeof(struct high_lvl_command) * num_hl_commands);
+    
+    int k;
+    //populate high lvl commands with command ptrs and input/output lists
+    for (k = 0; (command = read_command_stream (command_stream)); k++)
+    {
+            if( k >= num_hl_commands)
+            {
+                num_hl_commands += 100;
+                command_list = realloc(command_list, sizeof(struct high_lvl_command) * num_hl_commands);
+            }
+            command_list[k].command = command;
+            consolidate_io(command_list[k].inputs,command_list[k].outputs);
+    }
+    
+    //TODO:figure out how this whole thing will work???
     while ((command = read_command_stream (command_stream)))
     {
         if (print_tree)
